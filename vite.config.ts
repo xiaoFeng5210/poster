f2655024bde572c407c93f757a9ba6ebd3da253e
Eadweard <804875728@qq.com>
2023-02-01 21:40:14 +0800

    项目初始化

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path";
+import Unocss from 'unocss/vite'
+import { presetUno, presetAttributify, presetIcons } from 'unocss'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),
+  Unocss({
+    presets: [
+      presetUno(),
+      presetAttributify(),
+      presetIcons({
+        // 其他选项
+        prefix: 'i-',
+        extraProperties: {
+          display: 'inline-block'
+        }
+      })],
+  }),],
+  resolve: {
+    alias: {
+      '~': `${path.resolve(__dirname, 'src')}/`,
+      "components": path.resolve(__dirname, "src/components"),
+      "pages": path.resolve(__dirname, "src/pages"),
+      "utils": path.resolve(__dirname, "src/utils"),
+      "assets": path.resolve(__dirname, "src/assets"),
+      "composable": path.resolve(__dirname, "src/composable"),
+      "styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+})
