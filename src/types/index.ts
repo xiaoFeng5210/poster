69edec2f0059c96afdcff07f12b36a2211b41198
Eadweard <804875728@qq.com>
2023-06-02 11:33:17 +0800

    fix types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
-import Rect from "~/lib/Rect"
+import Rect from '~/lib/Rect'
 
 export { Rect }
 export type ObjectArg = Rect
-export type { Options as RectOptions } from "~/lib/Rect"
+export type { Options as RectOptions } from '~/lib/Rect'
