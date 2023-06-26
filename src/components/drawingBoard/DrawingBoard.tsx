b6e5e09e60a5a33d2e206dfb067d28cf1dba984b
Eadweard <804875728@qq.com>
2023-06-26 14:09:53 +0800

    refactor

--- src/components/drawingBoard/DrawingBoard.tsx
+++ src/components/drawingBoard/DrawingBoard.tsx
@@ -4,51 +4,50 @@ import { Spin } from '@arco-design/web-react'
 import { canvas_id, canvas_outer } from '~/config/settings'
 import { useKonvaStore } from '~/store/knovaStore'
 
+interface BoxStyles {
+  left: number
+  top: number
+  transformOrigin: string
+  transform: string
+}
 const DrawingBoard: FC = () => {
   const [loading, setLoading] = useState(true)
-  const changeLoading = (status: boolean) => {
-    setLoading(status)
-  }
   const initKonva = useKonvaStore(state => state.init)
   const [scale, size] = useKonvaStore(state => [state.scale, state.size])
-  const [outSize, setOutSize] = useState({ width: 0, height: 0 })
-  useEffect(() => {
+  let canvasBoxStyles: BoxStyles | {} = {}
+  const setOutBoxSize = ($box: HTMLElement) => {
+    if ($box.clientWidth && $box.clientHeight) {
+      const outWidth = $box.clientWidth
+      const outHeight = $box.clientHeight
+      const left = (outWidth - size.width) / 2
+      const top = (outHeight - size.height) / 2
+      canvasBoxStyles = {
+        left,
+        top,
+        transformOrigin: 'center',
+        transform: `scale(${scale}) `,
+      }
+      setLoading(false)
+    }
+  }
+  const initDashboard = () => {
     // 获取画布外面元素的宽高
     const $box = document.getElementById(canvas_outer)!
-    setOutSize({
-      width: $box.clientWidth,
-      height: $box.clientHeight,
-    })
-  }, [])
+    initKonva($box.clientWidth, $box.clientHeight)
+    setOutBoxSize($box)
+  }
   useEffect(() => {
-    if (!loading)
-      initKonva(outSize.width, outSize.height)
+    initDashboard()
+  }, [])
+  const showStyles = useMemo(() => {
+    return loading ? { display: 'none' } : { display: 'block' }
   }, [loading])
-  const canvasBoxStyles = useMemo(() => {
-    const left = (outSize.width - size.width) / 2
-    const top = (outSize.height - size.height) / 2
-    if (outSize.width || outSize.height)
-      changeLoading(false)
-    return {
-      left,
-      top,
-      transformOrigin: 'center',
-      transform: `scale(${scale}) `,
-    }
-  }, [outSize, scale])
   return (
     <div id={canvas_outer} className='flex-1 canvas_out relative overflow-hidden'>
-      {
-        loading
-          ? (
-          <Spin tip='This may take a while' loading={loading} style={{ display: 'block', height: '100%', marginTop: '400px' }} />
-            )
-          : (
-          <div className='absolute canvas_box' style={canvasBoxStyles}>
-            <div id={canvas_id} className=" bg-white" ></div>
-          </div>
-            )
-      }
+      <Spin tip='This may take a while' loading={loading} style={{ width: '100%', height: '100%', marginTop: '400px' }} />
+      <div className='absolute canvas_box' style={{ ...canvasBoxStyles, ...showStyles }}>
+        <div id={canvas_id} className=" bg-white" ></div>
+      </div>
     </div>
   )
 }
