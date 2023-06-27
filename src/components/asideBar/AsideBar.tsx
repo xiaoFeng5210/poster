f6fe4c8e6272f197aa3037718c1a0ce1e4267d4b
Eadweard <804875728@qq.com>
2023-06-27 11:40:10 +0800

    修复bug

--- src/components/asideBar/AsideBar.tsx
+++ src/components/asideBar/AsideBar.tsx
@@ -17,12 +17,12 @@ const AsideBar: FC = () => {
       {
         options.map((item, index) => {
           return (
-            <Trigger showArrow
+            <Trigger showArrow key={index}
               trigger='click'
               position='rt'
               popup={() => (<div className="w-100px h-100px bg-white">1233333</div>)}
               >
-              <li key={index} className="hover:text-#4080FF hover:color-#4080FF">
+              <li className="hover:text-#4080FF hover:color-#4080FF">
                 <i className={`${item.icon} text-30px p-y-5px`}></i>
                 <span>{item.name}</span>
               </li>
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'uno.css'
-import "@arco-design/web-react/dist/css/arco.css";
+import '@arco-design/web-react/dist/css/arco.css'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <App />,
+  // </React.StrictMode>,
 )
