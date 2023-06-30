df7625f9f483b1614d2ec24eab58cd2e812456b2
Eadweard <804875728@qq.com>
2023-06-30 11:23:07 +0800

    完善transformer

--- src/lib/konvaUse/transformer.ts
+++ src/lib/konvaUse/transformer.ts
@@ -1,21 +1,22 @@
 import Konva from 'konva'
 
 export const createTransformer = (stage: Konva.Stage, layer: Konva.Layer): Konva.Transformer => {
-  stage.findOne('Transformer').destroy()
   const tr = new Konva.Transformer()
   layer.add(tr)
   return tr
 }
 
 export const listenTransformerEvents = (stage: Konva.Stage, layer: Konva.Layer) => {
+  const tr = createTransformer(stage, layer)
   stage.on('click tap', (e) => {
     if (e.target === stage) {
-      stage.findOne('Transformer').destroy()
-      layer.draw()
+      tr.nodes([])
+      return
     }
-    const tr = createTransformer(stage, layer)
-    tr.attachTo(e.target)
-    layer.draw()
+    const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey
+    const isSelected = tr.nodes().includes(e.target)
+    if (!metaPressed && !isSelected)
+      tr.nodes([e.target])
   })
 }
 
