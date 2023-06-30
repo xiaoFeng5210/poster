4f01805a7017633dc1bdd430d70a82c12b3ee41a
Eadweard <804875728@qq.com>
2023-06-30 12:36:08 +0800

    fix

--- src/lib/konvaUse/transformer.ts
+++ src/lib/konvaUse/transformer.ts
@@ -1,13 +1,13 @@
 import Konva from 'konva'
 
-export const createTransformer = (stage: Konva.Stage, layer: Konva.Layer): Konva.Transformer => {
+export const createTransformer = (layer: Konva.Layer): Konva.Transformer => {
   const tr = new Konva.Transformer()
   layer.add(tr)
   return tr
 }
 
 export const listenTransformerEvents = (stage: Konva.Stage, layer: Konva.Layer) => {
-  const tr = createTransformer(stage, layer)
+  const tr = createTransformer(layer)
   stage.on('click tap', (e) => {
     if (e.target === stage) {
       tr.nodes([])
@@ -23,7 +23,7 @@ export const listenTransformerEvents = (stage: Konva.Stage, layer: Konva.Layer) 
 export const useAttachTransformer = (stage: Konva.Stage, layer: Konva.Layer) => {
   return (id: string) => {
     if (stage.findOne(`#${id}`)) {
-      const tr = createTransformer(stage, layer)
+      const tr = createTransformer(layer)
       tr.attachTo(stage.findOne(`#${id}`))
     }
   }
